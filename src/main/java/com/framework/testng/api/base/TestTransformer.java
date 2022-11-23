13eca5383251c63402f2a795b9fae791cb043ca1
Sailesh <saileshkumar17893@gmail.com>
2022-11-23 12:39:45 +0530

    Pattern java class added

--- src/main/java/com/framework/testng/api/base/TestTransformer.java
+++ src/main/java/com/framework/testng/api/base/TestTransformer.java
@@ -8,7 +8,7 @@ import org.testng.annotations.ITestAnnotation;
 
 public class TestTransformer implements IAnnotationTransformer {
 	
-	 public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+	 public void transform (ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
 		  annotation.setRetryAnalyzer(RetryEngine.class);
 		  }
 
